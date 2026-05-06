27d6d61273247932280a519866304db815dbb5ed
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2026-05-05 21:59:54 -0600

    Create ICampusContactRepository.cs 05-05-2026
    The ICampusContactRepository.cs interface was created for the functionality of viewing contacts

---
+++ UNAPLANNER-API/Repositories/ICampusContactRepository.cs
@@ -0,0 +1,10 @@
+using UNAPLANNER_API.Models.Entities;
+
+namespace UNAPLANNER_API.Repositories;
+
+public interface ICampusContactRepository
+{
+    Task<List<CampusContact>> GetAllAsync();
+    Task<List<CampusContact>> GetByCampusIdAsync(int campusId);
+    Task<CampusContact?> GetByIdAsync(int id);
+}
