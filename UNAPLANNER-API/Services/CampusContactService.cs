e020a90676606fedcd046109d20435fafadad591
KeynaCaFu <keyna.castro.fuentes@est.una.ac.cr>
2026-05-05 22:00:48 -0600

    Create CampusContactService.cs 05-05-2026
    The service CampusContactService.cs was created for the functionality of viewing contacts

---
+++ UNAPLANNER-API/Services/CampusContactService.cs
@@ -0,0 +1,35 @@
+using UNAPLANNER_API.DTOs.Responses;
+using UNAPLANNER_API.Mappers;
+using UNAPLANNER_API.Repositories;
+
+namespace UNAPLANNER_API.Services;
+
+public class CampusContactService : ICampusContactService
+{
+    private readonly ICampusContactRepository _campusContactRepository;
+
+    public CampusContactService(ICampusContactRepository campusContactRepository)
+    {
+        _campusContactRepository = campusContactRepository;
+    }
+
+    public async Task<List<CampusContactResponse>> GetAllContactsAsync()
+    {
+        var contacts = await _campusContactRepository.GetAllAsync();
+        return CampusContactMapper.ToCampusContactResponseList(contacts);
+    }
+
+    public async Task<List<CampusContactResponse>> GetContactsByCampusAsync(int campusId)
+    {
+        var contacts = await _campusContactRepository.GetByCampusIdAsync(campusId);
+        return CampusContactMapper.ToCampusContactResponseList(contacts);
+    }
+
+    public async Task<CampusContactResponse?> GetContactByIdAsync(int id)
+    {
+        var contact = await _campusContactRepository.GetByIdAsync(id);
+        if (contact == null) return null;
+        
+        return CampusContactMapper.ToCampusContactResponse(contact);
+    }
+}
