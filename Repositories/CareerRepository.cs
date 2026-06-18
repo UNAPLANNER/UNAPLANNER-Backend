4395473844386930df49434c27b96aacab943176
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-06-17 19:01:29 -0600

    Changes implements
    I made changes to Add a restriction to the API for retrieving careers, which displays, based on the campusId of the logged-in user, the campus of the registered user.

--- Repositories/CareerRepository.cs
+++ Repositories/CareerRepository.cs
@@ -25,6 +25,18 @@ public class CareerRepository : ICareerRepository
             .ToListAsync();
     }
 
+    public async Task<List<Career>> GetByCampusIdAsync(int campusId)
+    {
+        return await _context.Careers
+            .AsNoTracking()
+            .Include(c => c.Campus)
+            .Include(c => c.StudyPlans)
+                .ThenInclude(sp => sp.StudyPlanCourses)
+            .Where(c => c.IsStatus && c.CampusId == campusId)
+            .OrderBy(c => c.Name)
+            .ToListAsync();
+    }
+
     public async Task<Career?> GetByIdAsync(int id)
     {
         return await _context.Careers
