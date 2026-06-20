9d33ddb4d71a5e9262109a5653b050a6d9e8fcf5
adrianaOM3 <111209153+adrianaOM3@users.noreply.github.com>
2026-06-20 01:43:13 -0600

    Issue edit student perfil backend (#23)
    * implement user profile update endpoint with validations
    
    Add update profile functionality with basic validations
    
    * Update mapping and services
    
    The DTOs, mappers, services, and repositories have been updated to map and return the student's major and email address

--- Controllers/UserStudentController.cs
+++ Controllers/UserStudentController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using UNAPLANNER_API.DTOs.Requests;
 using UNAPLANNER_API.DTOs.Responses;
 using UNAPLANNER_API.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace UNAPLANNER_API.Controllers;
 
@@ -50,6 +51,63 @@ public class UserStudentController : ControllerBase
         {
             return StatusCode(StatusCodes.Status500InternalServerError,
                 new { message = "Error al eliminar el usuario", error = ex.Message });
+        }
+    }
+    [Authorize]
+    /// Retrieves a student's profile using the UserId
+    [HttpGet("{userId}/profile")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetProfile(int userId)
+    {
+        try
+        {
+            var result = await _userStudentService.GetStudentProfile(userId);
+
+            if (result == null)
+                return NotFound(new { message = "Estudiante no encontrado" });
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new { message = "Error al obtener perfil", error = ex.Message });
+        }
+    }
+
+    /**Updates the profile information of a student by userId.
+    The ID of the user whose profile will be updated
+    Returns 200 OK if the update is successful,
+    400 BadRequest if the data is invalid or the student is not found,
+    500 InternalServerError if an unexpected error occurs**/
+
+    [Authorize]
+    [HttpPut("{userId}/profile")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> UpdateProfile(int userId, [FromBody] UpdateStudentProfileRequest dto)
+    {
+        try
+        {
+            var result = await _userStudentService.UpdateProfileStudent(userId, dto);
+
+            if (result == null)
+                return BadRequest(new { message = "Datos inválidos o estudiante no encontrado" });
+
+            return Ok(new
+            {
+                message = "Perfil actualizado exitosamente",
+                data = result
+            });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new { message = "Error updating profile", error = ex.Message });
         }
     }
 }
