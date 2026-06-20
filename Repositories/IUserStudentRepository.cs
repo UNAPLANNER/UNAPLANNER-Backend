ecd15da4aa66932ca9f9817ea4399a5d274ae3d7
Natalia Ortiz Martinez <deyaneira.ortiz.martinez@est.una.ac.cr>
2026-06-20 02:00:48 -0600

    Revert "Issue edit student perfil backend (#23)"
    This reverts commit 9d33ddb4d71a5e9262109a5653b050a6d9e8fcf5.

--- Repositories/IUserStudentRepository.cs
+++ Repositories/IUserStudentRepository.cs
@@ -7,11 +7,4 @@ public interface IUserStudentRepository
     Task<User?> GetByIdUserStudent(int id);
     // Deletes a student user after validating the current password
     Task<bool> DeleteUserStudent(int id, string currentPassword);
-    // Carrer Validation
-    Task<bool> CareerExists(int careerId);
-    // STUDENT (UPDATE PROFILE)
-    Task<Student?> UpdateStudentProfile(Student student);
-    Task<Student?> GetStudentByUserId(int userId);
-    Task<Student?> GetStudentByUserIdWithUser(int userId);
-    
 }
